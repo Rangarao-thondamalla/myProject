257153976c70b3fba11df48d2a1f07353bbc92f7
Rangarao Thondamalla <t.rangarao007@gmail.com>
2021-11-17 09:01:43 +0530

    component interaction, child to parent , parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  myname = 'Rangarao';
+  number = 12345769;
+  my_array = ['Rajesh','guru','jaypal','ranga','hiran','raghu', 'lakshmimadam'];
 
+  mywifenameparent="Pulluru";
+  mywifenameinparent(valueEmited){
+    this.mywifenameparent = valueEmited;
+  }
+
+  counterintialvalue = 0;
+  counterincrements(value){
+    this.counterintialvalue = value;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input , Output , EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @Input() name : string;
+  @Input() lastname : string;
+  @Input() rolnumber : number ;
+  @Input() array : any[];
+
+
+  mywifename = "lakshmi";
+
+  counter = 1;
+
+  @Output() onClick  = new EventEmitter();
+  @Output() counterincrement = new EventEmitter();
+
+  onClicked(mywifename){
+    this.onClick.emit(mywifename);
+  }
+
+  counterincrese(counter){
+    this.counterincrement.emit(counter)
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
  show = true;
-
- my_array = ['Rajesh','guru','jaypal','ranga','hiran','raghu', 'lakshmimadam'];
 }
